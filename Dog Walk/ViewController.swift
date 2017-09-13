e82bfcb0a81ad5ceacc3e2b187c045b7cb882c8d
MS1 <ms1@MS1s-iMac.local>
2017-09-13 12:15:44 +0400

    Completed

--- Dog Walk/ViewController.swift
+++ Dog Walk/ViewController.swift
@@ -21,9 +21,12 @@
  */
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+  
+  var managedContext : NSManagedObjectContext!
+  
   // MARK: - Properties
   lazy var dateFormatter: DateFormatter = {
     let formatter = DateFormatter()
@@ -31,7 +34,7 @@ class ViewController: UIViewController {
     formatter.timeStyle = .medium
     return formatter
   }()
-  var walks: [NSDate] = []
+  var currentDog: Dog?
 
   // MARK: - IBOutlets
   @IBOutlet var tableView: UITableView!
@@ -39,6 +42,25 @@ class ViewController: UIViewController {
   // MARK: - View Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    let dogName = "Fido"
+    let dogFetch: NSFetchRequest<Dog> = Dog.fetchRequest()
+    dogFetch.predicate = NSPredicate(format: "%K == %@", #keyPath(Dog.name),
+                                     dogName)
+    do {
+      let results = try managedContext.fetch(dogFetch)
+      if results.count > 0 {
+        // Fido found, use Fido
+        currentDog = results.first
+      } else {
+        // Fido not found, create Fido
+        currentDog = Dog(context: managedContext)
+        currentDog?.name = dogName
+        try managedContext.save()
+      }
+    } catch let error as NSError {
+      print("Fetch error: \(error) description: \(error.userInfo)")
+    }
 
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
   }
@@ -48,7 +70,18 @@ class ViewController: UIViewController {
 extension ViewController {
 
   @IBAction func add(_ sender: UIBarButtonItem) {
-    walks.append(NSDate())
+    
+    let walk = Walk(context: managedContext)
+    walk.date = NSDate()
+    currentDog?.addToWalks(walk)
+    
+    tableView.reloadData()
+    
+    do {
+      try managedContext.save()
+    } catch let error as NSError {
+      print("Save error: \(error), description\(error.userInfo)")
+    }
     tableView.reloadData()
   }
 }
@@ -57,14 +90,33 @@ extension ViewController {
 extension ViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let walks = currentDog?.walks else { return 1 }
     return walks.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let date = walks[indexPath.row]
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    cell.textLabel?.text = dateFormatter.string(from: date as Date)
+    
+    guard let walk = currentDog?.walks?[indexPath.row] as? Walk,
+      let walkDate = walk.date as Date? else {
+        return cell }
+    cell.textLabel?.text = dateFormatter.string(from: walkDate)
     return cell
+  }
+  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    guard let walktoRemove = currentDog?.walks?[indexPath.row] as? Walk, editingStyle == .delete else { return }
+    managedContext.delete(walktoRemove)
+    
+    do {
+      try managedContext.save()
+      
+      tableView.deleteRows(at: [indexPath], with: .automatic)
+    } catch let error  as NSError{
+      print(error.localizedDescription)
+    }
   }
 
   func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
